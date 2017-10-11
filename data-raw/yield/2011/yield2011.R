4e90aaf1416b336524628b52fbe63cd87907ff71
Jarad Niemi <niemi@iastate.edu>
2017-10-11 17:25:31 -0500

    Modify yield2011.R
    doesn't currently work, use sf package

--- data-raw/yield/2011/yield2011.R
+++ data-raw/yield/2011/yield2011.R
@@ -1,10 +1,21 @@
-library(maptools)
+library("dplyr")
 
-Basswood <- readShapeSpatial("Basswood-2011-SBYield",
-                      proj4string = CRS("+GCS_WGS_1984"))
-Interim <- readShapeSpatial("Interim-2011-SBYield",
-                             proj4string = CRS("+GCS_WGS_1984"))
-Orbweaver <- readShapeSpatial("Orbweaver-2011-SBYield",
-                              proj4string = CRS("+GCS_WGS_1984"))
+my_read_shape = function(f, into) {
+  rgdal::readOGR(f, proj4string = CRS("+GCS_WGS_1984")) %>%
+    mutate(file=f) %>%
+    tidyr::separate(file, into)
+}
 
-yield2011 <- rbind(Basswood,Interim,Orbweaver)
+read_dir = function(path, pattern, into) {
+  files_w_ext = list.files(path = path,
+                     pattern = pattern,
+                     recursive = TRUE,
+                     full.names = TRUE)
+  files = gsub(".shp", "", files_w_ext)
+  plyr::ldply(files, my_read_shape, into = into)
+}
+
+
+yield <- read_dir(path = ".",
+                  pattern = "*.shp",
+                  into = c("year","watershed","year2","yield","ext"))
